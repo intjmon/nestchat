6d8de0d33a9de2328678d049a2b705bba243ab07
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-02 22:46:46 +0900

    mongodb 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NestModule } from '@nestjs/common/interfaces/modules';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 모든 모듈에서 사용할 수 있도록 설정. 다른모듈에서 가져올 필요없음
     }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
   ],
   controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
