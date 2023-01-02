3fecdefa58e1607bc4644e0aa863d27f854864b1
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-02 22:42:38 +0900

    환경변수를 전역으로 사용하기 위한 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 사용할 수 있도록 설정. 다른모듈에서 가져올 필요없음
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
