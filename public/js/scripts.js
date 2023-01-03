b01aa9daa48617c4f42e78241b4f8ff30d67fbb8
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-03 21:51:11 +0900

    html의 id로 찾아서 초기화함

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,13 @@
-alert('hello world');
+const getElementById = (id) => document.getElementById(id) || null;
+
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  const username = prompt('what is your name?');
+}
+function init() {
+  helloUser();
+}
+init();
