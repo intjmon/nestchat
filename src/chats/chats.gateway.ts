5bc0470763977cd2a0e1b30e514777135b9243fa
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-03 22:29:21 +0900

    new_user를 구독하고 그에 대한 응답을 보내는 코드를 추가

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,10 +1,22 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 // gateway로 소켓프로그램을 만들어서 사용할 수 있음
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('new_user')
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log(socket.id);
+    console.log(username);
+    socket.emit('hello_user', 'hello ' + username);
+    return 'hello world';
   }
 }
