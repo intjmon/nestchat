be59a2205e068dfb00361f9889737556d868384d
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-03 19:50:43 +0900

    mongodb 설정과 디버깅 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NestModule } from '@nestjs/common/interfaces/modules';
+import * as mongoose from 'mongoose';
 
+//console.log('MONGODB::::', process.env.MONGODB_URI);
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 모든 모듈에서 사용할 수 있도록 설정. 다른모듈에서 가져올 필요없음
     }),
-    MongooseModule.forRoot(process.env.MONGO_URL, {
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -19,4 +21,10 @@ import { NestModule } from '@nestjs/common/interfaces/modules';
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    console.log('MONGODB::::', process.env.MONGODB_URI);
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
