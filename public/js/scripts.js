eb513c2adeabae0d0d91ee3bb00f0b5928348237
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-08 16:03:21 +0900

    채팅 화면 관련 설정

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,12 +10,38 @@ const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+const handleSubmit = (event) => {
+  event.preventDefault(); // 폼에서 submit 이벤트가 발생하면 새로고침이 되는데 이를 막아준다.
+  //console.log('helloworld');
+  const inputValue = event.target.elements[0].value; // formElement[0]은 input 태그를 의미
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue); // send_msg 이벤트를 발생시키고 서버로 데이터를 전송
+    drawChatting(`me: ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
+
+socket.on('new_chat', (msg) => { // user_connected 이벤트를 받으면 실행
+  //  console.log(`${username} is connected`);
+  console.log('msg:', msg);
+  if (msg.chat !== '') {
+    drawChatting(`[${msg.username}: ${msg.chat}]`);
+  }
+});
+
 socket.on('user_connected', (username) => { // user_connected 이벤트를 받으면 실행
   console.log(`${username} is connected`);
 });
 // 화며에 그려주기 함수
 const drawHelloStranger = (username) =>
   (helloStrangerElement.innerText = `Hello ${username} :)`);
+
+const drawChatting = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = ` <div> ${message} </div>`;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
+};
 
 
 function helloUser() {
@@ -27,6 +53,7 @@ function helloUser() {
 
 function init() {
   helloUser();
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
