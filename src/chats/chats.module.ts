08bde873f62f692f1dcbb8044c97bfdf8cea170e
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-08 20:38:36 +0900

    chat gateway에서 chat, socketModel의 스키마와 모델을 사용하기위해 의존성을 추가해줌

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Chatting, ChattingSchama } from './models/chattings.model';
+import { SocketSchema, Socket as SocketModel } from './models/sockets.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Chatting.name, schema: ChattingSchama },
+      // SocketModel이라고 한 이유는 Socket.io에서 Socket이라는
+      // 이름을 사용하기 때문에 혼란을 피하기위함
+      { name: SocketModel.name, schema: SocketSchema },
+    ]),
+  ],
   providers: [ChatsGateway],
 })
 export class ChatsModule {}
