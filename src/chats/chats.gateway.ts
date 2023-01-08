83a4ecc2735be7ce96e7807985ce77156e68e1c3
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-08 11:50:51 +0900

    웹소켓의 이름이 있는경우는 게이트웨이에서 이름을 지정

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -6,8 +6,10 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
+// websocket의 네임스페이스
 // gateway로 소켓프로그램을 만들어서 사용할 수 있음
-@WebSocketGateway()
+//@WebSocketGateway() // 웹소캣의 네이스페이스가 없는 경우
+@WebSocketGateway({ namespace: 'linuxlinux' }) // 웹소캣의 네임스페이스가 있는 경우
 export class ChatsGateway {
   @SubscribeMessage('new_user')
   handleNewUser(
