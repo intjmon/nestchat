78ebc33260a772e3c836de4a00a581109ce54601
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-08 20:40:00 +0900

    socket 스키마 정의

--- src/chats/models/sockets.model.ts
+++ src/chats/models/sockets.model.ts
@@ -1,10 +1,29 @@
-import { Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Document, SchemaOptions } from 'mongoose';
 
 const options: SchemaOptions = {
-  collection: 'chattings',
+  id: false,
+  collection: 'sockets',
   timestamps: true, // db에 타임스템프를 자동으로 추가
 };
 
 @Schema(options)
-export class Socket extends Document {}
-export const SeocktSchema = SchemaFactory.createForClass(Socket);
+export class Socket extends Document {
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+}
+
+export const SocketSchema = SchemaFactory.createForClass(Socket);
